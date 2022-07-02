f685315b29d3de8b37d60e990849be71283326e5
PedroVignotto <pedro.vignotto@hotmail.com>
2022-07-02 16:00:44 -0300

    test: ensure Home enable submit button if form is valid

--- tests/application/pages/login/login.spec.tsx
+++ tests/application/pages/login/login.spec.tsx
@@ -1,14 +1,29 @@
+import { generateRandomOrder } from '@/tests/mocks'
 import { Home } from '@/application/pages'
 
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import React from 'react'
 
 describe('Home', () => {
+  let trackingCode: string
+
+  beforeAll(() => {
+    trackingCode = generateRandomOrder().trackingCode
+  })
+
   it('Should load with correct initial state', () => {
     render(<Home />)
 
     expect(screen.getByTestId('submit-button')).toHaveTextContent('Rastrear')
     expect(screen.getByTestId('submit-button')).toBeDisabled()
     expect(screen.getByTestId('trackingCode')).toHaveTextContent('')
+  })
+
+  it('Should enable submit button if form is valid', () => {
+    render(<Home />)
+
+    fireEvent.input(screen.getByTestId('trackingCode'), { target: { value: trackingCode } })
+
+    expect(screen.getByTestId('submit-button')).toBeEnabled()
   })
 })
